bf4f2afd52c42352c670215b1dc3bae152f4d237
wangchong <2421672443@qq.com>
2019-10-13 22:27:26 +0800

    添加idea工程到git的本地仓库

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello,pro");
+        System.out.println("hello,pro");
+        System.out.println("hello,pro");
+        System.out.println("hello,pro");
+        System.out.println("hello,pro");
+        System.out.println("hello,pro");
+    }
+}
